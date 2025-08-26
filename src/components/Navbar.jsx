94a1a59ab371d7fd6f1dd237562547dbf27dff31
Tam Le <lengoctam0106@gmail.com>
2025-08-26 11:16:15 -0400

    Navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <a href="#home" className='flex items-center gap-2'>
+                <img src="/images/logo.png" alt="logo" />
+                <p>Velvet Pour</p>
+            </a>
+
+            <ul>
+                {navLinks.map((link) => (
+                    <li key={link.id}>
+                        <a href={`#${link.id}`}>{link.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
